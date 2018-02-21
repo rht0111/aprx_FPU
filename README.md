9fe30e5001a6fff018b3eb71edda27447a358327
rht0111 <rohitprasad0111@gmail.com>
2018-02-21 16:23:06 +0100

    1st

---
+++ README.md
@@ -0,0 +1,12 @@
+# aprx_FPU
+
+approximate floating point unit (add and mul) based on binary16 alt and binary 8
+
+A Transprecision Floating-Point Platform for Ultra-Low Power Computing
+https://arxiv.org/abs/1711.10374
+
+add unit:
+
+mul unit:
+
+aprx_FPU:
---
+++ aprx_FPU_v1.sv
@@ -0,0 +1,12 @@
+/*		 _   / '_/ 
+ *		/ ()/)/ /                   
+ *                           
+ *   design 	:  approximate FPU (binary16 alt & binary8 
+ *				   A Transprecision Floating-Point Platform for Ultra-Low Power Computing 
+ *				   https://arxiv.org/abs/1711.10374
+ * 				   https://github.com/pulp-platform/riscv )                 
+ *   date		:  21.02.2018                      
+ *   version	:  1.0                      
+ *                           
+ */
+
